88de1eff01949fb027780659551adf57653cde0a
amateur-spelling <162478706+amateur-spelling@users.noreply.github.com>
2024-04-15 10:30:39 -0500

    4/15

---
+++ prog 122h/prog 122h/MainForm.py
@@ -0,0 +1,48 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._listBox1 = System.Windows.Forms.ListBox()
+		self._button1 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# listBox1
+		# 
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.Location = System.Drawing.Point(13, 13)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(591, 355)
+		self._listBox1.TabIndex = 0
+		# 
+		# button1
+		# 
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(646, 124)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(230, 138)
+		self._button1.TabIndex = 1
+		self._button1.Text = "calulate"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(931, 411)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._listBox1)
+		self.Name = "MainForm"
+		self.Text = "prog 122h"
+		self.ResumeLayout(False)
+
+
+	def Button1Click(self, sender, e):
+		heading = " Number \t Square \t Square Root \t Cube \t Square Cube
+		self_listBox1.Items.Add(heading)
+		
---
+++ slices/slices/MainForm.py
@@ -0,0 +1,51 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.Location = System.Drawing.Point(92, 94)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(75, 23)
+		self._button1.TabIndex = 0
+		self._button1.Text = "button1"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(284, 261)
+		self.Controls.Add(self._button1)
+		self.Name = "MainForm"
+		self.Text = "slices"
+		self.ResumeLayout(False)
+
+
+	def Button1Click(self, sender, e):
+		list1 = [1, 2, 3, 4, 5,]
+		list2 = list1[0:3] 
+		print(list2)
+		list3 = list1[3:len(list1)] # [3:]
+		print(list3)
+		print((list2 + list3) == list1)
+		
+		strHi = "Hello, world!"
+		substr = strHi [:5] 
+		print (substr)
+		print (strHi[-1])
+		print (strHi[-6:-1])
+		print (strHi[::-1])
+		print (strHi == strHi [::-1])
+		
+		
+		input()
